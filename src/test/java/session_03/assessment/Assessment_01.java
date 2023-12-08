9307fd6f62659923f1f17256dbe54b2065e11d20
lobna <elqattanlobna4@gmail.com>
2023-12-08 17:23:31 +0200

    session3

--- src/test/java/session_03/assessment/Assessment_01.java
+++ src/test/java/session_03/assessment/Assessment_01.java
@@ -1,5 +1,10 @@
 package session_03.assessment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assessment_01 {
 
     /*
@@ -11,6 +16,16 @@ public class Assessment_01 {
      */
 
     public static void main(String[] args) {
+
+WebDriver driver;
+driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.navigate().to("https://www.saucedemo.com/v1/");
+driver.findElement(By.id("user-name")).sendKeys("standard_user"+ Keys.TAB+"secret_sauce"+Keys.ENTER);
+
+
+
+
 
 
         // 1. Open URL: https://www.saucedemo.com/v1/
--- src/test/java/session_03/assessment/Assessment_02.java
+++ src/test/java/session_03/assessment/Assessment_02.java
@@ -1,10 +1,16 @@
 package session_03.assessment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Assessment_02 {
 
     /*
     Test Case No1 : Validate that user can log in successfully
+
         1. Open URL: https://www.saucedemo.com/v1/
+
         2. Enter Invalid username: standard
         3. Enter Invalid Password: secret
         4. Click On Login
@@ -13,7 +19,15 @@ public class Assessment_02 {
         
      */
 
+
     public static void main(String[] args) {
+        WebDriver driver;
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.saucedemo.com/v1/");
+        driver.findElement(By.id("user-name")).sendKeys("standard");
+        driver.findElement(By   .id("password")).sendKeys("secret");
+        driver.findElement(By.id("login-button")).click();
 
 
         // 1. Open URL: https://www.saucedemo.com/v1/
