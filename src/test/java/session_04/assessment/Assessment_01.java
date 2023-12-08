79b26befd5d6e387f72d83222c1fbe7ae3fb6236
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2023-12-08 20:36:18 +0200

    update assessment for session04

--- src/test/java/session_04/assessment/Assessment_01.java
+++ src/test/java/session_04/assessment/Assessment_01.java
@@ -18,9 +18,36 @@ public class Assessment_01 {
             3. Choose make Audi
             4. Choose model A1
             5. Print all result fount
+            6. Click on product Card contains "Audi A1 1.4 TFSI S line Sportback S Tronic"
      */
 
     public static void main(String[] args) {
+        WebDriver driver;
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.navigate().to("https://www.cazoo.co.uk/");
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("button[class=\"cui__sc-mqi3p7-0 fuyLT\"]")))).click();
+
+        Select makeDropDownMenu = new Select(driver.findElement(By.name("make")));
+        makeDropDownMenu.selectByValue("audi");
+
+        Select modelDropDownMenu = new Select(driver.findElement(By.name("model")));
+        modelDropDownMenu.selectByValue("a1");
+
+        driver.findElement(By.cssSelector("button[data-test-id='buy-form-btn']")).click();
+
+        for (int i = 1; i < driver.findElements(By.className("vehicle-cardstyles__LinkWrap-sc-1bxv5iu-5")).size() ; i++){
+
+            System.out.println(driver.findElements(By.className("vehicle-cardstyles__LinkWrap-sc-1bxv5iu-5")).get(i).getText());
+        }
+
+        driver.quit();
 
 
     }
