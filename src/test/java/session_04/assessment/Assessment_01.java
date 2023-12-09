e558bf0d5f2d542253c5a35eecf96b8c2ae833fd
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2023-12-09 10:22:53 +0200

    minor update

--- src/test/java/session_04/assessment/Assessment_01.java
+++ src/test/java/session_04/assessment/Assessment_01.java
@@ -23,38 +23,6 @@ public class Assessment_01 {
 
     public static void main(String[] args) {
 
-        
+
     }
 }
-
-/*
-        Model Answer:
-
-        WebDriver driver;
-
-                driver = new ChromeDriver();
-
-                driver.manage().window().maximize();
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-                driver.navigate().to("https://www.cazoo.co.uk/");
-
-                WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-                wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("button[class=\"cui__sc-mqi3p7-0 fuyLT\"]")))).click();
-
-                Select makeDropDownMenu = new Select(driver.findElement(By.name("make")));
-                makeDropDownMenu.selectByValue("audi");
-
-                Select modelDropDownMenu = new Select(driver.findElement(By.name("model")));
-                modelDropDownMenu.selectByValue("a1");
-
-                driver.findElement(By.cssSelector("button[data-test-id='buy-form-btn']")).click();
-
-                for (int i = 1; i < driver.findElements(By.className("vehicle-cardstyles__LinkWrap-sc-1bxv5iu-5")).size() ; i++){
-
-                    System.out.println(driver.findElements(By.className("vehicle-cardstyles__LinkWrap-sc-1bxv5iu-5")).get(i).getText());
-                }
-
-                driver.quit();
-            }
-         */
