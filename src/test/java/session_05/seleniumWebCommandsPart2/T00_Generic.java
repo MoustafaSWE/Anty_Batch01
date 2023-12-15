c96e2a8c08163a4a4bfb35412665634d7994c919
MoustafaSWE <moustafa.mo.ismail@gmail.com>
2023-12-15 22:20:57 +0200

    session 5

--- src/test/java/session_05/seleniumWebCommandsPart2/T00_Generic.java
+++ src/test/java/session_05/seleniumWebCommandsPart2/T00_Generic.java
@@ -12,6 +12,25 @@ import java.time.Duration;
 public class T00_Generic {
 
     public static void main(String[] args) {
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+
+        String rgbColor = driver.findElement(By.id("flash")).getCssValue("background-color");
+        System.out.println(rgbColor);
+
+        String hexColor = Color.fromString(rgbColor).asHex();
+        System.out.println(hexColor);
+
+
+
 
 
     }
