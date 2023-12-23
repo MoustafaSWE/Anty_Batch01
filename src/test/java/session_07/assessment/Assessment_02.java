68b268f281f8af776e77a83b2195f88b9abb21bf
fatma.abdelfattah <mfmaefh@gmail.com>
2023-12-23 08:01:46 +0200

    Fatma-session7

--- src/test/java/session_07/assessment/Assessment_02.java
+++ src/test/java/session_07/assessment/Assessment_02.java
@@ -1,5 +1,15 @@
 package session_07.assessment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 /*
     Scenario 1 (Verify that user can search for mazda mx-5)
         ● Navigate to https://www.ebay.com/
@@ -10,4 +20,34 @@ package session_07.assessment;
         ● From the left hand side filter using "Transmission" -> "Manual"
  */
 public class Assessment_02 {
-}
+    WebDriver driver;
+    String expectedCarName = "mazda mx-5";
+
+        @BeforeMethod
+        public void beforeMethod() {
+            driver= new ChromeDriver();
+            driver.navigate().to("https://www.ebay.com/");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4000));
+        }
+
+        @Test
+        public void test1(){
+            Assert.assertEquals(driver.getCurrentUrl(),"https://www.ebay.com/");
+            WebElement searchButton = driver.findElement(By.cssSelector("input[type=\"text\"]"));
+            searchButton.sendKeys("mazda mx-5");
+            driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+            int findName = driver.findElements(By.xpath("div[class=\"s-item__info clearfix\"]")).size();
+            for(int y=0; y < findName ; y++){
+                if (driver.findElements(By.xpath("div[class=\"s-item__info clearfix\"]")).get(y).getText().contains("expectedCarName");
+                break;
+            }
+
+
+
+
+            }
+
+        }
+
+
